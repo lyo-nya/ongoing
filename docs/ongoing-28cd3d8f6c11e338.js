d3e764663ede46869c9f5f55e90fdf2dd3a30fec
Leonid Zhizhin <Leonid.Zhizhin@jetbrains.com>
2026-01-16 16:37:54 +0100

    Rebuilt the app

--- docs/ongoing-28cd3d8f6c11e338.js
+++ docs/ongoing-28cd3d8f6c11e338.js
@@ -321,20 +321,20 @@ if (!('encodeInto' in cachedTextEncoder)) {
 
 let WASM_VECTOR_LEN = 0;
 
-function __wasm_bindgen_func_elem_85740(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_85740(arg0, arg1, addHeapObject(arg2));
-}
-
-function __wasm_bindgen_func_elem_88548(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_88548(arg0, arg1, isLikeNone(arg2) ? 0 : addHeapObject(arg2));
-}
-
 function __wasm_bindgen_func_elem_84494(arg0, arg1) {
     wasm.__wasm_bindgen_func_elem_84494(arg0, arg1);
 }
 
+function __wasm_bindgen_func_elem_85740(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_85740(arg0, arg1, addHeapObject(arg2));
+}
+
 function __wasm_bindgen_func_elem_81045(arg0, arg1, arg2, arg3) {
     wasm.__wasm_bindgen_func_elem_81045(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
+}
+
+function __wasm_bindgen_func_elem_88548(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_88548(arg0, arg1, isLikeNone(arg2) ? 0 : addHeapObject(arg2));
 }
 
 const __wbindgen_enum_GamepadMappingType = ["", "standard"];
